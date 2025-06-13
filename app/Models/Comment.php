4f29a291fc6b28a1d03e3f67ee8bd5103d0510c3
LAMPEngineer <lamp.engineer@gmail.com>
2025-06-13 13:02:16 +0530

    Relationsship one-to-many

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
@@ -13,5 +14,23 @@ class Comment extends Model
     protected $casts = [
         'body' => 'array',
     ];
+
+    /**
+     * Stablishes a relationship between the Comment and Post models.
+     *
+     */
     
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(related: Post::class);
+    }
+    
+    /**
+     * Stablishes a relationship between the Comment and User models.
+     *
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(related: User::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -14,4 +15,8 @@ class Post extends Model
         'body' => 'array',
     ];
     
+    public function comments(): HasMany
+    {
+        return $this->hasMany(related:Comment::class);
+    }
 }
